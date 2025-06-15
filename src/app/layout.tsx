16c0dbc7d9de3dbdebbb612a922f9bb2ebaef7ce
Altay Simsek <altaysimsek16@gmail.com>
2025-06-15 03:35:14 +0300

    Clarity kütüphanesi kaldırıldı ve yerine Microsoft Clarity'nin script entegrasyonu eklendi. package.json ve package-lock.json dosyalarında gerekli güncellemeler yapıldı.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { AuthProvider } from "@/contexts/auth-context";
 import { getUserSession } from "@/lib/auth";
-import Clarity from '@microsoft/clarity';
+import Script from "next/script";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -20,7 +20,6 @@ export const metadata: Metadata = {
   description: "Kafesium",
 };
 
-Clarity.init('rzm30f63xa');
 
 export default async function RootLayout({
   children,
@@ -33,6 +32,19 @@ export default async function RootLayout({
 
   return (
     <html lang="en">
+      <Script
+      id="microsoft-clarity-init"
+      strategy="afterInteractive"
+      dangerouslySetInnerHTML={{
+          __html: `
+          (function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "rzm30f63xa");
+          `,
+      }}
+      />
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased min-h-screen bg-gradient-to-br from-zinc-900 to-black text-white"div className="min-h-screen bg-gradient-to-br from-zinc-900 to-black text-white`}
       >
